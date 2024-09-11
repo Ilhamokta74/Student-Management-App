a99f7c3fc42a50d0661b3e3a43343a516082e814
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-09-11 11:01:14 +0700

    Part 2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: "Lato", sans-serif;
+        }
+
+        .sidebar {
+            margin: 0;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+            position: fixed;
+            height: 100%;
+            overflow: auto;
+        }
+
+        .sidebar a {
+            display: block;
+            color: black;
+            padding: 16px;
+            text-decoration: none;
+        }
+
+        .sidebar a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+
+        .sidebar a:hover:not(.active) {
+            background-color: #555;
+            color: white;
+        }
+
+        div.content {
+            margin-left: 200px;
+            padding: 1px 16px;
+            height: 1000px;
+        }
+
+        @media screen and (max-width: 700px) {
+            .sidebar {
+                width: 100%;
+                height: auto;
+                position: relative;
+            }
+
+            .sidebar a {
+                float: left;
+            }
+
+            div.content {
+                margin-left: 0;
+            }
+        }
+
+        @media screen and (max-width: 400px) {
+            .sidebar a {
+                text-align: center;
+                float: none;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -20,6 +82,25 @@
                         <span class="navbar-toggler-icon"></span>
                     </button>
                 </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="sidebar">
+                    <a href="#home" class="active">Home</a>
+                    <a href="#news">Student</a>
+                    <a href="#contact">Teacher</a>
+                    <a href="#about">Courses</a>
+                    <a href="#contact">Enrollment</a>
+                    <a href="#about">Payment</a>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <div class="content">
+                   @yield('content')
+                </div>
             </div>
         </div>
     </div>
